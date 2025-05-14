9d6abacbb055e67e77df95fc1cf79807c86afd82
kylejohnson-dev <160078497+kylejohnson-dev@users.noreply.github.com>
2025-05-13 21:06:21 -0500

    added dialog to update user profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Navbar } from "@/components/navbar";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="max-w-[1170px] min-h-screen px-4 mx-auto font-[family-name:var(--font-geist-sans)]">
       <Navbar />
-      <main className="space-y-14">
+      <main className="pb-16 md:pb-20">
         {/* hero section */}
         <div className="text-center">
           <div className="my-12 lg:my-16">
@@ -13,36 +14,98 @@ export default function Home() {
             <p className="text-preset-1 text-neutral-900">How are you feelin today?</p>
             <p className="text-preset-6 text-neutral-600">Wednesday, April 16th, 2025</p>
           </div>
-          <Button className="h-[60px] bg-blue-600 text-preset-5 text-white px-8 py-4">Log today&#39;s mood</Button>
+          <Button className="h-[60px] bg-blue-600 text-preset-5 text-white px-8 py-4 mb-12 lg:mb-16">Log today&#39;s mood</Button>
         </div>
         {/*  */}
-        <section className="grid grid-cols-1 lg:grid-cols-12 gap-8">
-          <div className="lg:col-span-4 bg-white rounded-2xl space-y-6 px-4 py-5">
-            <div className="space-y-4">
-              <p className="text-preset-5 text-neutral-900">
-                Average Mood
-                <span className="text-preset-7 text-neutral-600">(Last 5 Check-ins)</span>
-              </p>
-              <div className="h-[150px] place-content-center bg-blue-100 rounded-2xl space-y-3 px-4 py-5">
-                <p className="text-preset-4 text-neutral-900">Keep tracking!</p>
-                <span className="text-preset-7 text-neutral-900">Log 5 check-ins to see your average mood.</span>
+        <div className="space-y-8">
+          <section className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+            <div className="relative md:flex md:flex-col md:justify-between lg:col-span-8 md:h-[340px] bg-white rounded-2xl md:overflow-hidden space-y-6 px-4 py-8 md:p-8">
+              <span className="block w-full text-center md:text-left">
+                <span className="text-preset-3 text-neutral-900/75">I&#39;m feeling</span>
+                <br />
+                <span className="text-preset-2 text-neutral-900">Very Happy</span>
+              </span>
+              <Image 
+                src="/icon-very-happy-color.svg"
+                alt="happy"
+                width={200}
+                height={200}
+                className="md:absolute md:right-10 md:top-[50px] md:w-[320px] md:h-[320px] mx-auto"
+              />
+              <div className="flex flex-col items-center md:items-baseline justify-center space-y-4">
+                <Image 
+                  src="/icon-quote.svg"
+                  alt="quote"
+                  width={24}
+                  height={24}
+                />
+                <p className="text-preset-6-italic text-neutral-90 text-center md:text-left">&#34;When your hear is full, share<br className="hidden md:block" /> your light with the world.&#34;</p>
               </div>
             </div>
-            <div className="space-y-4">
-              <p className="text-preset-5 text-neutral-900">
-                Average Sleep
-                <span className="text-preset-7 text-neutral-600">(Last 5 Check-ins)</span>
-              </p>
-              <div className="h-[150px] place-content-center bg-blue-100 rounded-2xl space-y-3 px-4 py-5">
-                <p className="text-preset-4 text-neutral-900">Not enough data yet!</p>
-                <span className="text-preset-7 text-neutral-900">Track 5 nights to view average sleep.</span>
+            <div className="lg:col-span-4 space-y-5">
+              <div className="bg-white rounded-2xl space-y-4 p-5">
+                <span className="flex items-center space-x-3">
+                  <Image 
+                    src="/icon-sleep.svg"
+                    alt="sleep"
+                    width={19}
+                    height={22}
+                    className="fill-neutral-600"
+                  />
+                  <span className="text-preset-6 text-neutral-600">Sleep</span>
+                </span>
+                <p className="text-preset-3 text-neutral-900">9+ hours</p>
+              </div>
+              <div className="bg-white rounded-2xl space-y-4 p-5">
+                <span className="flex items-center space-x-3">
+                  <Image 
+                    src="/icon-reflection.svg"
+                    alt="exercise"
+                    width={22}
+                    height={22}
+                    className=""
+                  />
+                  <span className="text-preset-6 text-neutral-600">Reflection of the day</span>
+                </span>
+                <div className="h-20">
+                  <p className="text-preset-6 text-neutral-900">Woke up early and finally tackled a big project!</p>
+                </div>
+                <div className="flex items-center space-x-3">
+                  <span className="text-preset-6-italic text-neutral-600">#Gratful</span>
+                  <span className="text-preset-6-italic text-neutral-600">#Optimistic</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="lg:col-span-8 bg-white rounded-2xl">
+          </section>
+          {/*  */}
+          <section className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+            <div className="lg:col-span-4 bg-white rounded-2xl space-y-6 px-4 py-5">
+              <div className="space-y-4">
+                <p className="text-preset-5 text-neutral-900">
+                  Average Mood
+                  <span className="text-preset-7 text-neutral-600">(Last 5 Check-ins)</span>
+                </p>
+                <div className="h-[150px] place-content-center bg-blue-100 rounded-2xl space-y-3 px-4 py-5">
+                  <p className="text-preset-4 text-neutral-900">Keep tracking!</p>
+                  <span className="text-preset-7 text-neutral-900">Log 5 check-ins to see your average mood.</span>
+                </div>
+              </div>
+              <div className="space-y-4">
+                <p className="text-preset-5 text-neutral-900">
+                  Average Sleep
+                  <span className="text-preset-7 text-neutral-600">(Last 5 Check-ins)</span>
+                </p>
+                <div className="h-[150px] place-content-center bg-blue-100 rounded-2xl space-y-3 px-4 py-5">
+                  <p className="text-preset-4 text-neutral-900">Not enough data yet!</p>
+                  <span className="text-preset-7 text-neutral-900">Track 5 nights to view average sleep.</span>
+                </div>
+              </div>
+            </div>
+            <div className="lg:col-span-8 bg-white rounded-2xl">
 
-          </div>
-        </section>
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,11 @@
+import { DialogTitle } from "@radix-ui/react-dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTrigger } from "./ui/dialog";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import Image from "next/image";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 function Navbar() {
   return (
@@ -14,54 +18,89 @@ function Navbar() {
           height={40}
         />
       </div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost">
-            <Avatar className="w-10 h-10">
-              <AvatarImage src="/avatar-lisa.jpg" alt="lisa avatar" />
-              <AvatarFallback>LM</AvatarFallback>
-            </Avatar>
-            <Image 
-              src="/icon-dropdown-arrow.svg"
-              alt="arrow down"
-              width={11}
-              height={6}
-              className="ml-2"
-            />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" sideOffset={16} className="w-[calc(100vw-64px)] md:w-[200px]">
-          <DropdownMenuLabel className="text-preset-6 text-nuetral-900">
-            Lisa Maria
-            <br />
-            <span className="text-preset-7 text-neutral-300">lisa@mail.com</span>
-          </DropdownMenuLabel>
-          {/* <span className="text-preset-7 text-neutral-300">lisa@mail.com</span> */}
-          <DropdownMenuSeparator className="my-1.5" />
-          <DropdownMenuGroup>
-            <DropdownMenuItem className="flex items-center space-x-2.5">
+      <Dialog>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <Avatar className="w-10 h-10">
+                <AvatarImage src="/avatar-lisa.jpg" alt="lisa avatar" />
+                <AvatarFallback>LM</AvatarFallback>
+              </Avatar>
               <Image 
-                src="/icon-settings.svg"
-                alt="settings"
-                width={16}
-                height={16}
-                className="fill-neutral-900"
+                src="/icon-dropdown-arrow.svg"
+                alt="arrow down"
+                width={11}
+                height={6}
+                className="ml-2"
               />
-              <span className="text-preset-7 text-neutral-900">Settings</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="flex items-center space-x-2.5">
-              <Image 
-                src="/icon-logout.svg"
-                alt="settings"
-                width={16}
-                height={16}
-                className="fill-neutral-900"
-              />
-              <span className="text-preset-7 text-neutral-900">Logout</span>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" sideOffset={16} className="w-[calc(100vw-32px)] md:w-[200px]">
+            <DropdownMenuLabel className="text-preset-6 text-nuetral-900">
+              Lisa Maria
+              <br />
+              <span className="text-preset-7 text-neutral-300">lisa@mail.com</span>
+            </DropdownMenuLabel>
+            {/* <span className="text-preset-7 text-neutral-300">lisa@mail.com</span> */}
+            <DropdownMenuSeparator className="my-1.5" />
+            <DropdownMenuGroup>
+              <DialogTrigger asChild>
+                <DropdownMenuItem className="flex items-center space-x-2.5">
+                  <Image 
+                    src="/icon-settings.svg"
+                    alt="settings"
+                    width={16}
+                    height={16}
+                    className="fill-neutral-900"
+                  />
+                  <span className="text-preset-7 text-neutral-900">Settings</span>
+                </DropdownMenuItem>
+              </DialogTrigger>
+              <DropdownMenuItem className="flex items-center space-x-2.5">
+                <Image 
+                  src="/icon-logout.svg"
+                  alt="settings"
+                  width={16}
+                  height={16}
+                  className="fill-neutral-900"
+                />
+                <span className="text-preset-7 text-neutral-900">Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* Dialog */}
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle className="text-preset-3 text-neutral-900">Update your profile</DialogTitle>
+            <DialogDescription className="text-preset-6 text-neutral-600">
+              Personalize your account with your name and photo.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="space-y-2">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input id="name" value="Lisa Maria" className="" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="avatar" className="text-right">
+                Avatar
+              </Label>
+              <div className="flex items-center space-x-2">
+                <Avatar>
+                  <AvatarImage src="/avatar-placeholder.svg" alt="avatar" />
+                </Avatar>
+                <Button variant="outline">Upload</Button>
+              </div>
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit" className="w-full h-[60px] text-preset-5 bg-blue-600 text-white">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </nav>
   );
 }
